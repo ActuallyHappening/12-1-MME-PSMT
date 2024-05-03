9dd92215e6a274f39406e0fb676e8a1ad128b8f0
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-05-03 11:45:54 +1000

    fix: debugs more

--- scratchspace/src/lib.rs
+++ scratchspace/src/lib.rs
@@ -1,5 +1,5 @@
 use scientific::{Precision, Scientific};
-use tracing::debug;
+use tracing::{debug, trace};
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
@@ -9,7 +9,7 @@ pub enum Error {
 	ConvertError(#[from] scientific::ConversionError),
 }
 
-pub fn trapezoidal_rule(
+pub fn trapezoidal_rule_strip_num(
 	func: impl Fn(f64) -> f64,
 	bottom_a: f64,
 	top_b: f64,
@@ -25,39 +25,60 @@ pub fn trapezoidal_rule(
 	let bottom_a = Scientific::try_from(bottom_a)?;
 
 	let strip_delta = (&top_b - &bottom_a).div_rpsp(&Scientific::from(number), precision)?;
+	debug!(%strip_delta, %bottom_a, %top_b, "Beginning trapezoidal rule calculation");
 
-	let ends = &func(&bottom_a)? + &func(&top_b)?;
+	let bottom = func(&bottom_a)?;
+	let top = func(&top_b)?;
+	debug!(%bottom, %top);
+	let ends = &bottom + &top;
 	let middle: Scientific = {
 		// let middle_num = number - 2;
 		let multiples = 1..number;
 		// assert_eq!(multiples.len(), middle_num as usize, "Multiples length is not equal to middle_num");
 
 		let mut sum = Scientific!(0);
+
+		#[cfg(feature = "debug")]
+		let mut m_values = Vec::new();
+
 		for m in multiples {
 			let m = Scientific::from(m);
 			let x = &bottom_a + &(&m * &strip_delta);
 			let strip_sum = func(&x)?;
-			debug!(%m, ?strip_sum, "Computing");
+			trace!(%m, ?strip_sum, "Computing");
+
+			#[cfg(feature = "debug")]
+			m_values.push(strip_sum.clone());
+
 			sum = &sum + &strip_sum;
 		}
 
-		debug!(%sum, "Finished sum");
+		trace!(%sum, "Finished sum");
+
+		#[cfg(feature = "debug")]
+		debug!(?m_values, "Middle values");
+
 		sum
 	};
 
 	let ret = &(strip_delta.div_rpsp(&Scientific!(2.0), precision))?
 		* &(&ends + &(&Scientific!(2.0) * &middle));
+	
+	debug!(%ret, "Finished trapezoidal rule calculation");
+
 	Ok((&ret).into())
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+	use tracing::info;
+
+use super::*;
 
 	#[test]
 	fn test_zero() {
 		let func = |_| 0.0;
-		let approx = trapezoidal_rule(func, 0.0, 10.0, 5, Precision::Digits(10)).unwrap();
+		let approx = trapezoidal_rule_strip_num(func, 0.0, 10.0, 5, Precision::Digits(10)).unwrap();
 		assert_eq!(approx, 0.0);
 	}
 
@@ -65,7 +86,17 @@ mod tests {
 	fn test_constant() {
 		let c = 6.9;
 		let func = |_| c;
-		let approx = trapezoidal_rule(func, 0.0, 10.0, 10, Precision::Digits(10)).unwrap();
+		let approx = trapezoidal_rule_strip_num(func, 0.0, 10.0, 10, Precision::Digits(10)).unwrap();
 		assert_eq!(approx, c * 10.0);
+	}
+
+	#[allow(dead_code)]
+	fn test_quadratic() {
+		let number = 10;
+		let precision = Precision::Digits(100);
+
+		let f = |x: f64| 6.0 * x.powi(2) + 4.0 * x - 7.0;
+		let sum = trapezoidal_rule_strip_num(f, 0.0, 1.0, number, precision).unwrap();
+		info!(test_sum = %sum);
 	}
 }
--- scratchspace/src/main.rs
+++ scratchspace/src/main.rs
@@ -1,18 +1,8 @@
-use std::f64::consts::{E, TAU};
+use std::{f64::consts::{E, TAU}, ops::Add};
 
 use scientific::Precision;
-use tracing::info;
-use ymath::trapezoidal_rule;
-
-#[allow(dead_code)]
-fn test_quadratic() {
-	let number = 10;
-	let precision = Precision::Digits(100);
-
-	let f = |x: f64| 6.0 * x.powi(2) + 4.0 * x - 7.0;
-	let sum = trapezoidal_rule(f, 0.0, 1.0, number, precision).unwrap();
-	info!(test_sum = %sum);
-}
+use tracing::{debug, info};
+use ymath::trapezoidal_rule_strip_num;
 
 #[allow(non_snake_case)]
 fn main() {
@@ -28,7 +18,10 @@ fn main() {
 	let t_t = |x: f64| 0.5 * ((TAU / 2.0) * x).sin().powf(0.5) + 0.5;
 	let t_b = |x: f64| -0.5 * ((TAU / 2.0) * x).sin().powf(0.5) + 0.5;
 	let g = 2.8025625;
-	let l = |x: f64| ((1.0 / 10.0) * x + 0.1).ln() + g;
+	let l = |x: f64| ((1.0 / 10.0) * x.add(1.0)).ln() + g;
+
+	let l_test = l(0.5);
+	debug!(l = %l_test, "L at x = 0.5");
 
 	let I = 0.6197;
 	let number = 20;
@@ -38,25 +31,34 @@ fn main() {
 	info!(%number, ?precision, "Computing ...");
 
 	let h_tl = 1.4;
-	let A_tl = trapezoidal_rule(|x| t_t(x) - l(x), 0.0, I, number, precision).unwrap();
+	let d_tl = |x| t_t(x) - l(x);
+	let d_tl_test = d_tl(0.5);
+	debug!(d_tl = %d_tl_test, "D at x = 0.5");
+	let A_tl = trapezoidal_rule_strip_num(d_tl, 0.0, 0.6, 12, precision).unwrap();
 	let V_tl = A_tl * w * h * h_tl;
-	info!(%A_tl, %V_tl);
+	info!(%A_tl, %V_tl, "proper way");
 
-	let h_m = 1.9;
-	let A_m = trapezoidal_rule(|x| l(x) - e_l(x), 0.0, 0.5, number, precision).unwrap()
-		+ trapezoidal_rule(|x| l(x) - e_r(x), 0.5, I, number, precision).unwrap()
-		+ trapezoidal_rule(|x| t_t(x) - e_r(x), I, 1.0, number, precision).unwrap();
-	let V_m = A_m * w * h * h_m;
-	info!(%A_m, %V_m);
+	// let h_tl = 1.4;
+	// let A_tl = trapezoidal_rule_strip_num(|x| t_t(x) - l(x), 0.0, I, number, precision).unwrap();
+	// let V_tl = A_tl * w * h * h_tl;
+	// info!(%A_tl, %V_tl);
 
-	let h_b = 0.5;
-	let A_b = trapezoidal_rule(|x| e_l(x) - t_b(x), 0.0, 0.5, number, precision).unwrap()
-		+ trapezoidal_rule(|x| e_r(x) - t_b(x), 0.5, 1.0, number, precision).unwrap();
-	let V_b = A_b * w * h * h_b;
-	info!(%A_b, %V_b);
+	// // T_{m}\left(x\right)=\left\{0\le x<I:l\left(x\right),I\le x\le1:t_{t}\left(x\right)\right\}
+	// let h_m = 1.9;
+	// let A_m = trapezoidal_rule_strip_num(|x| l(x) - e_l(x), 0.0, 0.5, number, precision).unwrap()
+	// 	+ trapezoidal_rule_strip_num(|x| l(x) - e_r(x), 0.5, I, number, precision).unwrap()
+	// 	+ trapezoidal_rule_strip_num(|x| t_t(x) - e_r(x), I, 1.0, number, precision).unwrap();
+	// let V_m = A_m * w * h * h_m;
+	// info!(%A_m, %V_m);
 
-	let V_t = V_tl + V_m + V_b;
-	info!(%V_t, "Final computed volume");
+	// let h_b = 0.5;
+	// let A_b = trapezoidal_rule_strip_num(|x| e_l(x) - t_b(x), 0.0, 0.5, number, precision).unwrap()
+	// 	+ trapezoidal_rule_strip_num(|x| e_r(x) - t_b(x), 0.5, 1.0, number, precision).unwrap();
+	// let V_b = A_b * w * h * h_b;
+	// info!(%A_b, %V_b);
+
+	// let V_t = V_tl + V_m + V_b;
+	// info!(%V_t, "Final computed volume");
 
 	info!("Finished");
 }
